9fa42a722015957b69e8fec805171f6d8b406629
wangchun <906103494@qq.com>
2018-10-17 00:04:49 +0800

    mnist tf 示例——读取minist数据

---
+++ 20180925/minist_tf.py
@@ -0,0 +1,11 @@
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("/Users/wangchun/Documents/project/machinelearn/tensorflow_learn/mnist_data",
+                                  one_hot=True,
+                                  source_url="http://yann.lecun.com/exdb/mnist/")
+
+print("训练数据集大小: ", mnist.train.num_examples)
+print("验证数据集大小: ", mnist.validation.num_examples)
+print("测试数据集大小: ", mnist.test.num_examples)
+print("训练数据示例: ", mnist.train.images[0])
+print("训练数据label: ", mnist.train.labels[0])
