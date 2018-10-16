ad7aa378414ed718199494b4e1251131fe60a575
wangchun <906103494@qq.com>
2018-10-17 00:07:34 +0800

    mnist tf 示例——批量训练

--- 20180925/minist_tf.py
+++ 20180925/minist_tf.py
@@ -9,3 +9,10 @@ print("验证数据集大小: ", mnist.validation.num_examples)
 print("测试数据集大小: ", mnist.test.num_examples)
 print("训练数据示例: ", mnist.train.images[0])
 print("训练数据label: ", mnist.train.labels[0])
+
+
+batch_size= 100
+xs, ys = mnist.train.next_batch(batch_size)
+
+print("X shape: ", xs.shape)
+print("Y shape: ", ys.shape)
